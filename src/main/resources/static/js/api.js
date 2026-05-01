be477a091fb91648f0eb9d27388c4b6d24b50fd6
withvivek <vy830237@gmail.com>
2026-05-02 01:29:24 +0530

    Fix corrupted frontend files and HTML structure

--- src/main/resources/static/js/api.js
+++ src/main/resources/static/js/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = '/api';
+const API_BASE_URL = 'http://localhost:8081/api';
 
 const apiFetch = async (endpoint, options = {}) => {
     const token = localStorage.getItem('token');
